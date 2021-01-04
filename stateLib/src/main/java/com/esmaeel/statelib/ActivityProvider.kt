268ec489103e66c4e36bf2d2dfbbb5894fa09310
Esmaeel Nabil <70523323+EsmaeelNabilM@users.noreply.github.com>
2021-01-04 15:42:28 +0200

    Fix comments

--- stateLib/src/main/java/com/esmaeel/statelib/ActivityProvider.kt
+++ stateLib/src/main/java/com/esmaeel/statelib/ActivityProvider.kt
@@ -5,23 +5,23 @@ import android.app.Application
 import android.os.Bundle
 
 /**
- * This changes from the NetworkStateHandler to give appHasNetwork
- * some credibility and to make sure it's a valid value without making it nullable
+ * This changes from the registerActivityLifecycleCallbacks to give currentActivity
+ * some credibility and to make sure it's a valid value.
  */
 var ACTIVITY_TRACKER_REGISTERED: Boolean = false
 
 /**
- *  A Global Network state variable to access from anywhere without
- *  the hassle of Injecting the Class in every consumer class.
+ *  A Global currentActivity variable to access from anywhere without
+ *  the hassle of Injecting a Class in every consumer class.
  *
- *  set(value) -> updates the value and makes ACTIVITY_TRACKER_REGISTERED true
- *                in order to Allow access to the Getter.
- *                the setter is private to prevent changes from outside this module
+ *  set(value) ->  -updates the value and makes ACTIVITY_TRACKER_REGISTERED true -
+ *                   in order to Allow access to the Getter.
+ *                - the setter is private to prevent changes from outside this module.
  *
- *  get ()     -> checks first for the `registerActivityTracker` being called in the Application class onCreate.
+ *  get ()     -> checks first for the `ACTIVITY_TRACKER_REGISTERED` is True thus registerActivityTracker has been called in the Application class's onCreate.
  *                in order to make it easy to access the global value.
  *
- *  default value is Null, cause it will be updated instantly if registerActivityTracker has been registered in application's onCreate
+ *  default value is Null, cause it will be updated instantly if registerActivityTracker has been registered in application's onCreate.
  */
 var currentActivity: Activity? = null
     private set(value) {
@@ -37,7 +37,7 @@ var currentActivity: Activity? = null
                         You should call registerActivityTracker()
                         inside the Application class in order to access this value globally.
                         Also you must not access currentActivity value in Application Class's onCreate,
-                        as it won't be accessible until the Tracker starts, an activity is being presented -
+                        as it won't be accessible until the registerActivityLifecycleCallbacks starts, an activity is being presented -
                         and the value gets updated.
                           
                     """
@@ -79,6 +79,7 @@ fun Application.registerActivityTracker(
          *  we could make the currentActivity Null again in these cases, but in case of
          *  the consumer started an activity then finished the previous one
          *  it will make the real currentActivity null due to the lifecycle calling order.
+         *  so overriding the currentvalue is enough. 
          */
         override fun onActivityPaused(activity: Activity) {
             onStateChanged?.invoke(activity)
